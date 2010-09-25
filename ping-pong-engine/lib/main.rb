5c7306605160df857cc60f9c1b16244f5ce46bef
Hartog C. de Mik <hartog.de.mik@simplic.it>
2010-09-25 18:21:33 +0000

    Some refactoring for the sake of readability and changed error handling

--- ping-pong-engine/lib/main.rb
+++ ping-pong-engine/lib/main.rb
@@ -15,9 +15,12 @@ require 'ruote-amqp'
 }
 
 
-puts AMQP.settings.inspect
+# puts AMQP.settings.inspect
+
+# uncomment for lots of usefull information
 # AMQP.logging = true
 
+# listen to the ruote_workitems queue for return-messages
 engine = Ruote::Engine.new(
   Ruote::Worker.new(
     Ruote::FsStorage.new("ruote_data")
@@ -30,7 +33,7 @@ receiver = RuoteAMQP::Receiver.new(
   :launchitems => false
 )
 
-# generice AMQP participant
+# generic AMQP participant
 engine.register_participant :amqp, RuoteAMQP::Participant
 
 # Puts state and count
@@ -39,23 +42,19 @@ engine.register_participant :logger do |workitem|
   $stderr.puts "Count: #{workitem.fields['count']}"
 end
 
-# Puts a workitem
-engine.register_participant :error_logger do |workitem|
-  $stderr.puts "Workitem: #{workitem.fields.inspect}"
-end
-
 # Set up the state and count
 engine.register_participant :alpha do |wi|
   wi.fields['state'] = 'alpha'
   wi.fields['count'] = 0
 end
 
+# make 2 seperate MQ subscribers play ping pong with each other
 pdef = Ruote.process_definition :name => 'simple' do
   # the main definition
   sequence :on_error => :shout do
     setup
     repeat do
-      run
+      play
       _break :if => '${f:count} >= 6'
     end
   end
@@ -67,7 +66,7 @@ pdef = Ruote.process_definition :name => 'simple' do
   end
 
   # perform a ping and a pong
-  define 'run' do
+  define 'play' do
     ping
     participant :logger
     pong
@@ -76,20 +75,32 @@ pdef = Ruote.process_definition :name => 'simple' do
 
   # do ping
   define 'ping' do
-    amqp :queue => 'ping', :command => '/ping/ping', :reply_queue => 'ruote_workitems'
+    amqp :queue       => 'ping',
+         :command     => '/ping/ping',
+         :reply_queue => 'ruote_workitems'
   end
 
   # do pong
   define 'pong' do
-    amqp :queue => 'pong', :command => '/pong/pong', :reply_queue => 'ruote_workitems'
+    amqp :queue       => 'pong',
+         :command     => '/pong/pong',
+         :reply_queue => 'ruote_workitems'
   end
 
   # trap errors and log 'em
   define :shout do
     echo 'Error occured...'
-    participant :error_logger
   end
 end
 
 wfid = engine.launch(pdef)
 engine.wait_for(wfid)
+
+# show the errors
+errs = engine.errors
+if errs.size > 0
+  puts "there are processes with errors :"
+  errs.each do |err|
+  puts "process #{err.wfid}"
+  end
+end
