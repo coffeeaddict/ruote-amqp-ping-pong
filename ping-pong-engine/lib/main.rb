0c225e3b87c3988d015f0dc1ef70ae2e6c1ebc41
Hartog C. de Mik <hartog.de.mik@simplic.it>
2010-09-26 19:04:14 +0000

    Refactored to reflect the blog post at http://simplic.it/5a19b5b

--- ping-pong-engine/lib/main.rb
+++ ping-pong-engine/lib/main.rb
@@ -5,7 +5,14 @@ require 'ruote'
 require 'ruote/storage/fs_storage'
 require 'ruote-amqp'
 
+# Setup an engine
+engine = Ruote::Engine.new(
+  Ruote::Worker.new(
+    Ruote::FsStorage.new("ruote_data")
+  )
+)
 
+# Configure AMQP
 { :host => 'localhost',
   :vhost => 'ruote',
   :user => 'ruote',
@@ -14,42 +21,33 @@ require 'ruote-amqp'
   AMQP.settings[k]=v
 }
 
-
-# puts AMQP.settings.inspect
-
 # uncomment for lots of usefull information
 # AMQP.logging = true
 
 # listen to the ruote_workitems queue for return-messages
-engine = Ruote::Engine.new(
-  Ruote::Worker.new(
-    Ruote::FsStorage.new("ruote_data")
-  )
-)
-
 receiver = RuoteAMQP::Receiver.new(
   engine,
-  :queue => "ruote_workitems",
   :launchitems => false
 )
 
-# generic AMQP participant
-engine.register_participant :amqp, RuoteAMQP::Participant
+# register ping and pong as participants to the game
+engine.register_participant :ping, RuoteAMQP::Participant, :queue => "ping"
+engine.register_participant :pong, RuoteAMQP::Participant, :queue => "pong"
 
-# Puts state and count
+# we need a crowd as well
 engine.register_participant :logger do |workitem|
   $stderr.puts "State: #{workitem.fields['state']}"
   $stderr.puts "Count: #{workitem.fields['count']}"
 end
 
-# Set up the state and count
+# and we need some one to initiate the game
 engine.register_participant :alpha do |wi|
   wi.fields['state'] = 'alpha'
   wi.fields['count'] = 0
 end
 
 # make 2 seperate MQ subscribers play ping pong with each other
-pdef = Ruote.process_definition :name => 'simple' do
+pdef = Ruote.process_definition :name => 'game' do
   # the main definition
   sequence :on_error => :shout do
     setup
@@ -59,35 +57,21 @@ pdef = Ruote.process_definition :name => 'simple' do
     end
   end
 
-  # setup shop 
+  # setup shop by calling alpha and let the logger show the state
   define 'setup' do
-    participant :alpha
-    participant :logger
+    alpha
+    logger
   end
 
   # perform a ping and a pong
   define 'play' do
-    ping
-    participant :logger
-    pong
-    participant :logger
+    ping :command => "/ping/ping", :reply_queue => "ruote_workitems"
+    logger
+    pong :command => "/pong/pong", :reply_queue => "ruote_workitems"
+    logger
   end
 
-  # do ping
-  define 'ping' do
-    amqp :queue       => 'ping',
-         :command     => '/ping/ping',
-         :reply_queue => 'ruote_workitems'
-  end
-
-  # do pong
-  define 'pong' do
-    amqp :queue       => 'pong',
-         :command     => '/pong/pong',
-         :reply_queue => 'ruote_workitems'
-  end
-
-  # trap errors and log 'em
+  # show the occurence of errors
   define :shout do
     echo 'Error occured...'
   end
